a907fa72b13cb81876270f68256ba23d0db42e30
Dante Gonzales <ronal.gonzales@tecsup.edu.pe>
2026-09-11 12:48:25 -0500

    feat: Add name TextField component

--- app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,12 +39,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "Bienvenido al Curso",
-        modifier = modifier,
-        fontSize = 28.sp,
-        fontWeight = FontWeight.Bold
-    )
+    var name by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        Text(
+            text = "Bienvenido al Curso",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Ingresa tu nombre") }
+        )
+    }
 }
 
 @Preview(showBackground = true)
